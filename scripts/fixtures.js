0a9f7b65d9cacd12c4f06a6c7a744d808580e997
Carl Truong <Carl@tabrific.com>
2016-03-04 11:13:34 -0700

    add jQuery functionality to next & previous buttons, add fixtures.js

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+	title: 'The Colors',
+	artist: 'Pablo Picasso',
+	label: 'Cubism',
+	year: '1881',
+	albumArtUrl: 'assets/images/album_covers/01.png',
+	songs: [
+		{ title: 'Blue', duration: '4:26' },
+		{ title: 'Green', duration: '3:14' },
+		{ title: 'Red', duration: '5:01' },
+		{ title: 'Pink', duration: '3:21' },
+		{ title: 'Magenta', duration: '2:15' }
+	]
+};
+
+var albumTidesOfMan = {
+	title: 'Young and Courageous',
+	artist: 'Tides of Man',
+	label: 'Firefly',
+	year: '2014',
+	albumArtUrl: 'assets/images/album_covers/11.png',
+	songs: [
+		{ title: 'Desolate. Magnificent.', duration: '3:18' },
+		{ title: 'Mountain House', duration: '2:52' },
+		{ title: 'Drift', duration: '5:05'},
+		{ title: 'Young and Courageous', duration: '5:39' },
+		{ title: 'All the Years', duration: '2:07'}
+	]
+};
+
+var albumMarconi = {
+	title: 'The Telephone',
+	artist: 'Guglielmo Marconi',
+	label: 'EM',
+	year: '1909',
+	albumArtUrl: 'assets/images/album_covers/20.png',
+	songs: [
+		{ title: 'Hello, Operator?', duration: '1:01' },
+		{ title: 'Ring, ring, ring', duration: '5:01' },
+		{ title: 'Fits in your pocket', duration: '3:21'},
+		{ title: 'Can you hear me now?', duration: '3:14' },
+		{ title: 'Wrong phone number', duration: '2:15'}
+	]
+};
